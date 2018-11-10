80328f353f846f996eea87e18446fa793a818b3c
fsaif <alsaif.fawzi@gmail.com>
2018-11-10 23:42:51 +0200

    fixed api routes

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::group([
     'prefix' => '/api'
 ], function () {
-    Route::post('/login', 'AuthController@login');
-    Route::post('/signup', 'AuthController@signup');
+    Route::post('/login', 'Api\AuthController@login');
+    Route::post('/signup', 'Api\AuthController@signup');
 
     Route::group([
         'middleware' => 'auth:api'
     ], function () {
-        Route::get('/logout', 'AuthController@logout');
-        Route::get('/user', 'AuthController@user');
+        Route::get('/logout', 'Api\AuthController@logout');
+        Route::get('/user', 'Api\AuthController@user');
     });
 });
